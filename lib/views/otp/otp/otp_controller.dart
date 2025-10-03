491e1a294e89904bca62e7e85c0a8d8e79308e42
Adnan Nahid <133763369+nahidhasanniloy@users.noreply.github.com>
2025-10-04 04:58:04 +0600

    loginm -register and otp complete

--- lib/views/otp/otp/otp_controller.dart
+++ lib/views/otp/otp/otp_controller.dart
@@ -7,12 +7,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 class OtpController extends GetxController {
   var isLoading = false.obs;
 
+
+
   Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString("auth_token", token);
-    print("✅ Token Saved: $token");
+     print("✅ Token Saved: $token");
   }
 
+
+  // API
   Future<void> verifyOtp(String email, String otp) async {
     try {
       isLoading.value = true;
@@ -27,17 +31,21 @@ class OtpController extends GetxController {
         final data = jsonDecode(response.body);
         print(response.body);
 
+
+
         if (data["status"] == true) {
           // ✅ Token Extract
-          String? token = data["token"]; 
+          Get.snackbar("Success", data["message"] ?? "OTP Verified ");
+          Get.toNamed(AppRoutes.LoginA);
+
+
+          String? token = data["token"];
 
           if (token != null) {
             await saveToken(token);
           }
 
-          Get.snackbar("Success", data["message"] ?? "OTP Verified ");
 
-          Get.toNamed(AppRoutes.LoginA);
         } else {
           Get.snackbar("Error", data["message"] ?? "Invalid OTP ");
         }
